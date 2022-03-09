5dd66d7d7f4d602a348f6deffb26059e7efd0b7e
VALHALL4 <hsm011007@naver.com>
2022-03-09 17:59:28 +0900

    Day5-3,5-F

---
+++ Odd_ConvenienceStore/Assets/scripts/Day5/Day5_3.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Day5_3 : MonoBehaviour
+{
+    [SerializeField] private Text txt_dialogue; //대화내용 담는 객체
+    [SerializeField] private Text txt_name;// 이름 담는 객체 
+    public static bool isDialogue = false;
+    public static int count = 0;
+    private List<Dictionary<string, object>> data;
+    public FadeInOut fade;
+
+
+
+    public void Showdialogue()// 처음시작할때 다 초기화하고 대화내용을 보여주는 함수
+    {
+        txt_dialogue.gameObject.SetActive(true);
+        txt_name.gameObject.SetActive(true);
+        count = 0;
+        isDialogue = true;
+        Nextdialogue();
+    }
+    public void Nextdialogue()//대화내용 넘기는 함수
+    {
+        Debug.Log(count);
+        txt_dialogue.gameObject.SetActive(true);
+        txt_name.gameObject.SetActive(true);
+        txt_name.text = data[count]["Name"].ToString();
+        data[count]["Script"] = data[count]["Script"].ToString().Replace("#", ",");
+        data[count]["Script"] = data[count]["Script"].ToString().Replace("  ", "\n");
+        txt_dialogue.text = data[count]["Script"].ToString();
+        count++;
+
+    }
+
+
+    private void Hidedialogue()//대화가 끝났으면 대화내용 숨기는 함수
+    {
+        txt_name.gameObject.SetActive(false);
+        txt_dialogue.gameObject.SetActive(false);
+        isDialogue = false;
+    }
+    private void Start()
+    {
+        data = CSVReader.Read("Day5-3");
+        Showdialogue();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDialogue)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (count < data.Count)
+                {
+                    Nextdialogue();
+                    if (count == 16)
+                    {
+                        count += 9;
+                    }
+                    if (count == 45)
+                    {
+                        count += 11;
+                    }
+                    if (count == 66)
+                    {
+                        count += 7;
+                    }
+                    if (count == 85)
+                    {
+                        count += 3;
+                    }
+
+
+
+                }
+                else
+                {
+                    fade.Fade();
+                    Hidedialogue();
+                }
+
+            }
+        }
+        else
+            Hidedialogue();
+    }
+}
---
+++ Odd_ConvenienceStore/Assets/scripts/Day5/Day5_C.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class day5choice
+{
+    [TextArea]
+    public string _choice;
+    public string _choice2;
+}
+
+public class Day5_C : MonoBehaviour
+{
+    [SerializeField] private Button choicebtn1;
+    [SerializeField] private Button choicebtn2;
+    [SerializeField] private Text txt_choice1;
+    [SerializeField] private Text txt_choice2;
+
+    public static int count = 0;
+    [SerializeField] private js4choice[] choice;
+    public void ShowChoice()
+    {
+        ChoiceOn();
+        txt_choice1.text = choice[count]._choice;
+        txt_choice2.text = choice[count]._choice2;
+    }
+    public void NextChoice1()
+    {
+        count++;
+        day5_3_1();
+        ChoiceOff();
+    }
+    public void NextChoice2()
+    {
+        count++;
+        day5_3_2();     
+        ChoiceOff();
+
+    }
+    public void ChoiceOn()
+    {
+        Day5_3.isDialogue = false;
+        Day5_F.isDialogue = false;
+        choicebtn1.gameObject.SetActive(true);
+        choicebtn2.gameObject.SetActive(true);
+        txt_choice1.gameObject.SetActive(true);
+        txt_choice2.gameObject.SetActive(true);
+    }
+    public void ChoiceOff()
+    {
+        Day5_3.isDialogue = true;
+        Day5_F.isDialogue = true;
+        choicebtn1.gameObject.SetActive(false);
+        choicebtn2.gameObject.SetActive(false);
+        txt_choice1.gameObject.SetActive(false);
+        txt_choice2.gameObject.SetActive(false);
+    }
+    private void day5_3()
+    {
+        if (Day5_3.count == 10)//일반3 첫번째 AB선택지
+        {
+            ShowChoice();
+        }
+        if (Day5_3.count == 33)//일반3 두번째 AB선택지
+        {
+            ShowChoice();
+        }
+        if (Day5_3.count == 57)//일반5 첫번째 AB선택지
+        {
+            ShowChoice();
+        }
+        if (Day5_3.count == 81)//진상2 첫번째 AB선택지
+        {
+            ShowChoice();
+        }
+        
+    }
+    private void day5_3_1()
+    {
+        if (Day5_3.count == 10)//일반3 AB선택지
+        {
+            Day5_3.count++;
+        }
+        if (Day5_3.count == 33)//일반3 두번째 A선택지
+        {
+            Day5_3.count++;
+        }
+        if (Day5_3.count == 57)//일반5 첫번째 A선택지
+        {
+            Day5_3.count++;
+        }
+        if (Day5_3.count == 81)//진상2 첫번째 A선택지
+        {
+            Day5_3.count++;
+        }
+        
+
+    }
+    private void day5_3_2()
+    {
+        if (Day5_3.count == 10)//일반3 AB선택지
+        {
+            Day5_3.count += 6;
+        }
+        if (Day5_3.count == 33)//일반3 두번째 B선택지
+        {
+            Day5_3.count += 12;
+        }
+        if (Day5_3.count == 57)//일반5 첫번째 B선택지
+        {
+            Day5_3.count += 9;
+        }
+        if (Day5_3.count == 81)//진상2 첫번째 B선택지
+        {
+            Day5_3.count += 4;
+        }
+        
+    }
+
+   
+    
+    
+    public void CallChoice()
+    {
+        day5_3();
+      
+    }
+    private void Start()
+    {
+        ChoiceOff();
+    }
+    private void Update()
+    {
+        CallChoice();
+    }
+}
