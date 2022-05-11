c7f26ad7dc01503773f3a81d17bf62f36b5625fe
Hyun_W <69100145+yehang218@users.noreply.github.com>
2022-05-11 20:15:25 +0900

    Day7 SaveData연동

--- Odd_ConvenienceStore/Assets/scripts/Day7/Day7_1.cs
+++ Odd_ConvenienceStore/Assets/scripts/Day7/Day7_1.cs
@@ -13,13 +13,11 @@ public class Day7_1 : MonoBehaviour
     public FadeInOut fade;
     public AudioClip touchclip;
 
-
-
     public void Showdialogue()// 처음시작할때 다 초기화하고 대화내용을 보여주는 함수
     {
         txt_dialogue.gameObject.SetActive(true);
         txt_name.gameObject.SetActive(true);
-        count = 0;
+        //count = 0;
         isDialogue = true;
         Nextdialogue();
     }
@@ -55,6 +53,14 @@ public class Day7_1 : MonoBehaviour
         txt_dialogue.gameObject.SetActive(false);
         isDialogue = false;
     }
+    public void Awake()
+    {
+        SaveData.DoLoadData = true;
+        SaveData.TempScene = "Day7-1";
+        count = SaveData.TempCount;
+        SaveData.Saves();
+    }
+
     private void Start()
     {
         data = CSVReader.Read("Day7-1");
@@ -92,7 +98,8 @@ public class Day7_1 : MonoBehaviour
                     fade.Fade();
                     Hidedialogue();
                 }
-
+                SaveData.TempCount = count - 1;
+                SaveData.Saves();
             }
         }
         else
