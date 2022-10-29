797c3eab1f259fd9df045da59c45504d6658a66c
VALHALL4 <hsm011007@naver.com>
2022-10-29 23:53:02 +0900

    bgm 적용

--- Odd_ConvenienceStore/Assets/scripts/SoundManager.cs
+++ Odd_ConvenienceStore/Assets/scripts/SoundManager.cs
@@ -2,25 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public static SoundManager instance;
     public AudioMixer mixer;
+    public AudioSource bgm;
+    public AudioClip[] bgmlist;
+
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(instance);
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
         else
         {
             Destroy(gameObject);
         }
     }
-
+   
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        if (SceneManager.GetActiveScene().name == "Title")
+            BGMplay(bgmlist[0]);
+        else
+            BGMplay(bgmlist[1]);
+    }
     public void SFXPlay(string sfxName, AudioClip clip)
     {
         GameObject go = new GameObject(sfxName + "Sound");
@@ -29,6 +41,16 @@ public class SoundManager : MonoBehaviour
         audioSource.clip = clip;
         audioSource.Play();
         Destroy(go, clip.length); 
+    }
+
+    public void BGMplay(AudioClip clip)
+    {
+        bgm.outputAudioMixerGroup = mixer.FindMatchingGroups("BGM")[0];
+        bgm.clip = clip;
+        bgm.loop = true;
+        bgm.volume = 0.1f;
+        bgm.Play();
+
     }
 
     public void SFXVolume(float val)
